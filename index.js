b47ccb280a3b06aa79df49dea6fa20e5435e567f
Benjamin Bordne <benjibordne@outlook.com>
2022-07-01 18:38:53 +0000

    added check for /

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ app.get('/url/:url', (req, res) => {
     const { url } = req.params;
     const url_new = url.substring(8);
 
-    console.log(url_new);
+    const regex = /\/$/gm;
+    const url_new_away = url_new.replace(regex, "");
+    console.log('URL: ', url_new_away);
+
 
     const options = {
     urls: [url],
-    directory: url_new,
+    directory: url_new_away,
     recursive: 1,
     maxRecursiveDepth: 2,
     maxDepth: 2,
@@ -24,13 +27,14 @@ app.get('/url/:url', (req, res) => {
 
     scrape(options).then((result) => {
 
-        zipFolder(url_new, 'public/' + url_new + '.zip', function(err) {
+        zipFolder(url_new_away, 'public/' + url_new_away + '.zip', function(err) {
             if(err) {
                 console.log('oh no!', err);
             } else {
+                console.log(url_new_away);
                 console.log('Oh God, Downloaded!');
-                fs.rmSync(url_new, { recursive: true, force: true });
-                res.send('http://download.benji.link:3000/' + url_new + '.zip')
+                fs.rmSync(url_new_away, { recursive: true, force: true });
+                res.send('http://download.benji.link:3000/' + url_new_away + '.zip')
             }
         });
 
@@ -38,13 +42,14 @@ app.get('/url/:url', (req, res) => {
 
     process.on('uncaughtException', (err) => {
         res.end("ERROR");
-        fs.rmSync(url_new, { recursive: true, force: true });
-        console.log('Caught exception: ', err);
+        fs.rmSync(url_new_away, { recursive: true, force: true });
+        console.log('Caught exception inside me: ', err);
     })
 
 });
 
 process.on('uncaughtException', (err) => {
+    console.log("AAAA")
     console.log('Caught exception: ', err);
 })
 
